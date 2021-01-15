6e97c1d51050b63921b9b57f24206f864418b877
2170987947 <2170987947@qq.com>
2021-01-15 11:00:22 +0800

    顺序表2

--- structure/src/Main.java
+++ structure/src/Main.java
@@ -6,16 +6,20 @@
 public class Main {
     public static void main(String[] args) {
         MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.addIndex(0,100);
         myLinkedList.addFirst(2);
         myLinkedList.addFirst(3);
-        myLinkedList.addFirst(4);
         myLinkedList.addEnd(5);
         myLinkedList.addEnd(6);
-        myLinkedList.addEnd(7);
-        myLinkedList.addIndex(0,10);
-        myLinkedList.addIndex(2,11);
-        myLinkedList.addIndex(8,12);
-        myLinkedList.addIndex(10,13);
+        //-1, 6 是非法插入
+        myLinkedList.addIndex(-1,10);
+        myLinkedList.addIndex(6,14);
+        //头插
+        myLinkedList.addIndex(0,11);
+        //尾插
+        myLinkedList.addIndex(6,12);
+        //中间插
+        myLinkedList.addIndex(5,13);
 
         myLinkedList.display();
     }
--- structure/src/MyLinkedList.java
+++ structure/src/MyLinkedList.java
@@ -52,39 +52,80 @@ public class MyLinkedList {
 
     //插入到某个位置
     public void addIndex(int index ,int data) {
+        if (index < 0 || index > this.getLength()) {
+            System.out.println("插入位置有误!");
+            return;
+        }
+        //第一个元素 : 头插
         if (index == 0) {
             addFirst(data);
             return;
         }
+        //最后一个元素 : 尾插
+        if (index == this.getLength()) {
+            addEnd(data);
+            return;
+        }
+        //中间元素 : 1.先找到插入点前一个元素结点. 2.再进行插入
         Node node = new Node(data);
+        //保存前一个节点
+        Node pre = searchPre(index);
+        node.next = pre.next;
+        pre.next = node;
+//        Node node = new Node(data);
 //        if (index == 0) {
 //            node.next = this.head;
 //            this.head = node;
 //            return;
 //        }
         //找到插入点前一个元素
-        Node cur = this.head;
-        int count = 0;
-        while (count < index - 1 && cur.next != null) {
-            cur = cur.next;
-            count++;
-            System.out.println(cur.data);
-        }
-        //同时满足采用尾插法
-        if (count == index - 1 && cur.next == null) {
-            addEnd(data);
-            return;
-        }
-        if (count == index - 1) {
-            node.next = cur.next;
-            cur.next = node;
-        }
-        if (count < index - 1) {
-            System.out.println("插入位置有误");
-        }
+//        Node cur = this.head;
+//        int count = 0;
+//        while (count < index - 1 && cur.next != null) {
+//            cur = cur.next;
+//            count++;
+//            System.out.println(cur.data);
+//        }
+//        //同时满足采用尾插法
+//        if (count == index - 1 && cur.next == null) {
+//            addEnd(data);
+//            return;
+//        }
+//        if (count == index - 1) {
+//            node.next = cur.next;
+//            cur.next = node;
+//        }
+//        if (count < index - 1) {
+//            System.out.println("插入位置有误");
+//        }
 
     }
-     //打印
+    //找 index 结点的前驱结点
+    public Node searchPre(int index) {
+        if (index <= 0 || index > getLength()) {
+            return null;
+        }
+        Node cur = this.head;
+        for (int i = 0; i < index - 1; i++) {
+            cur = cur.next;
+        }
+        return cur;
+    }
+    //计算单链表长度
+    public int getLength() {
+        if (this.head == null) {
+            return 0;
+        }
+        int count = 0;
+        Node node1 = this.head;
+        while (node1 != null) {
+            node1 = node1.next;
+            count++;
+        }
+        return count;
+    }
+
+    //打印
     public void display() {
         Node cur = this.head;
         while (cur != null) {
