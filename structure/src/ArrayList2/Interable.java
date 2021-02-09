2a012a5fdacae28a83dd03a04e47e0f32ebd35ff
2170987947 <2170987947@qq.com>
2021-02-09 23:50:57 +0800

    顺序表

---
+++ structure/src/ArrayList2/Interable.java
@@ -0,0 +1,9 @@
+package ArrayList2;
+
+/**
+ * @author FMM
+ * @version 7.0
+ * @date 2021/2/8 15:19
+ */
+public interface Interable extends Iterable{
+}
---
+++ structure/src/ArrayList2/Test.java
@@ -0,0 +1,14 @@
+package ArrayList2;
+
+import javax.crypto.spec.PSource;
+
+/**
+ * @author FMM
+ * @version 7.0
+ * @date 2021/2/8 15:20
+ */
+public class Test {
+    public static void main(String[] args) {
+
+    }
+}
