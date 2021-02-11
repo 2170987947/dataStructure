8f108361842b03a603683038df96f02f7d7b237c
2170987947 <2170987947@qq.com>
2021-02-11 23:40:47 +0800

    队转换成队

---
+++ structure/src/myQueue/QueueTransformStack.java
@@ -0,0 +1,46 @@
+package myQueue;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * @author FMM
+ * @version 7.0
+ * @date 2021/2/11 23:10
+ */
+public class QueueTransformStack {
+    Deque<Integer> stack = new LinkedList<>();
+
+    public void push(Integer e) {
+        this.stack.offer(e);
+    }
+    public Integer peek() {
+        for (int i = 0; i < this.stack.size() - 1; i++) {
+            Integer e = this.stack.pop();
+            this.stack.offer(e);
+        }
+        return this.stack.peek();
+    }
+    public Integer pop() {
+        for (int i = 0; i < this.stack.size() - 1; i++) {
+            Integer e = this.stack.pop();
+            this.stack.offer(e);
+        }
+        return this.stack.pop();
+    }
+
+    public static void main(String[] args) {
+        QueueTransformStack queueTransformStack = new QueueTransformStack();
+        queueTransformStack.stack.offer(1);
+        queueTransformStack.stack.offer(2);
+        queueTransformStack.stack.offer(3);
+        queueTransformStack.push(4);
+        System.out.println(queueTransformStack.pop());
+        System.out.println(queueTransformStack.pop());
+        System.out.println(queueTransformStack.pop());
+        System.out.println(queueTransformStack.pop());
+
+//        System.out.println(queueTransformStack.stack.pop());
+    }
+}
+
