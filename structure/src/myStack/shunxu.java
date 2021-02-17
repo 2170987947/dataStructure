49f2111ac28a0206a271d29b7ac86f77f9b8e141
2170987947 <2170987947@qq.com>
2021-02-17 08:59:13 +0800

    出栈顺序

---
+++ structure/src/myStack/shunxu.java
@@ -0,0 +1,20 @@
+package myStack;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * @author FMM
+ * @version 7.0
+ * @date 2021/2/17 8:54
+ */
+public class shunxu {
+    public Deque<Integer> stack1;
+    public Deque<Integer> stack2;
+
+    public void initStack() {
+        this.stack1 = new LinkedList<>();
+        this.stack2 = new LinkedList<>();
+    }
+    
+}
