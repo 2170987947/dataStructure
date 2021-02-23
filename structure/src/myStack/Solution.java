ee81e81ae21a9c19f6e59b567f480ce4132b76c9
2170987947 <2170987947@qq.com>
2021-02-23 09:18:53 +0800

    顺序

--- structure/src/myStack/Solution.java
+++ structure/src/myStack/Solution.java
@@ -1,7 +1,9 @@
 package myStack;
 
+import java.util.ArrayList;
 import java.util.Deque;
 import java.util.LinkedList;
+import java.util.List;
 
 /**
  * @author FMM
@@ -9,26 +11,40 @@ import java.util.LinkedList;
  * @date 2021/2/17 8:54
  */
 public class Solution {
-    public Deque<Integer> pushA;
-    public Deque<Integer> popA;
-
-
-    public void so(int[] pushA, int[] popA) {
+    // 重要程度排序:
+    private List<Integer> intArrayToList(int[] array) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < array.length; i++) {
+            list.add(array[i]);
+        }
+        return list;
+    }
+    public boolean isPoOrder(int[] pushA, int[] popA) {
+        List<Integer> pushList = intArrayToList(pushA);
+        List<Integer> popList = intArrayToList(popA);
         Deque<Integer> stack = new LinkedList<>();
-        while (popA != null) {
-            int temp = popA;
-            if (stack == null || ) {
-
-            } else {
-                
+        while (popList.isEmpty()) {
+            int p = popList.remove(0);
+            while (stack.isEmpty() || stack.peek() != p) {
+                if (stack.isEmpty() || stack.peek().intValue() != p) {
+                    if (pushList.isEmpty()) {
+                        return false;
+                    }
+                    Integer q = popList.remove(0);
+                    stack.push(q);
+                } else {
+                    break;
+                }
+                stack.pop();
             }
         }
     }
+
 
     public static void main(String[] args) {
         Solution solution = new Solution();
         int[] arr1 = {1, 2, 3, 4, 5};
         int[] arr2 = {3, 5, 4, 2, 1};
-        solution.so(arr1, arr2);
+        solution.isPoOrder(arr1, arr2);
     }
 }
