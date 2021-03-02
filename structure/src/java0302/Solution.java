f1b16513afea5e42cfdc78c3a3495baff29335a6
2170987947 <2170987947@qq.com>
2021-03-02 22:37:01 +0800

    赎金信

---
+++ structure/src/java0302/Solution.java
@@ -0,0 +1,27 @@
+package java0302;
+
+/**
+ * @author FMM
+ * @version 7.0
+ * @date 2021/3/2 22:02
+ */
+public class Solution {
+    public static boolean canConstruct(String ransom, String magazine) {
+        if (ransom.length() > magazine.length()) {
+            return false;
+        }
+        int[] cap = new int[26];
+        for (char c : ransom.toCharArray()) {
+            int index = magazine.indexOf(c, cap[c - 'a']);
+            if (index == -1) {
+                return false;
+            }
+            cap[c - 'a'] = index + 1;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(canConstruct("ads", "adacs"));
+    }
+}
