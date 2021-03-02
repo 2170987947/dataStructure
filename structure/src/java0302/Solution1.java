d04d3c41b5bce2a23bc20fc3a07d2681296992fe
2170987947 <2170987947@qq.com>
2021-03-02 22:59:03 +0800

    判断整数是否为回文数

---
+++ structure/src/java0302/Solution1.java
@@ -0,0 +1,25 @@
+package java0302;
+
+/**
+ * @author FMM
+ * @version 7.0
+ * @date 2021/3/2 22:42
+ */
+public class Solution1 {
+    public static boolean isPalindrome(int num) {
+        if (num < 0 || (num != 0 && num % 10 == 0)) {
+            return false;
+        }
+        int yuan = num;
+        int fan = 0;
+        while (yuan > fan) {
+            fan = fan * 10 + yuan % 10;
+            yuan /= 10;
+        }
+        return yuan == fan || yuan == fan / 10;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(1000));
+    }
+}
