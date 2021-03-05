fbd961f74e36f17bb0f38aca3750bb33ed43ca4a
2170987947 <2170987947@qq.com>
2021-03-06 01:00:57 +0800

    树的遍历

---
+++ structure/src/MyTree/TreeNode.java
@@ -0,0 +1,15 @@
+package MyTree;
+
+/**
+ * @author FMM
+ * @version 7.0
+ * @date 2021/3/5 15:35
+ */
+public class TreeNode {
+    char var;
+    TreeNode left;
+    TreeNode right;
+    public TreeNode(char var) {
+        this.var = var;
+    }
+}
---
+++ structure/src/MyTree/TreeTraversal.java
@@ -0,0 +1,51 @@
+package MyTree;
+
+/**
+ * @author FMM
+ * @version 7.0
+ * @date 2021/3/5 15:32
+ */
+public class TreeTraversal {
+    public static void preTraversal(TreeNode root) {
+        if (root != null) {
+            System.out.print(root.var);
+            preTraversal(root.left);
+            preTraversal(root.right);
+        } else {
+            // 空树
+        }
+    }
+    public static void inTraversal(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        inTraversal(root.left);
+        System.out.print(root.var);
+        inTraversal(root.right);
+    }
+    public static void postTraversal(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        postTraversal(root.left);
+        preTraversal(root.right);
+        System.out.print(root.var);
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode('A');
+        root.left = new TreeNode('B');
+        root.right = new TreeNode('C');
+        root.left.left = new TreeNode('D');
+        root.left.right = new TreeNode('E');
+        System.out.print("先序遍历: ");
+        preTraversal(root);
+        System.out.println();
+        System.out.print("中序遍历: ");
+        inTraversal(root);
+        System.out.println();
+        System.out.print("后序遍历: ");
+        postTraversal(root);
+        System.out.println();
+    }
+}
