3641a227116162a00b44b181ab8cd30beeaffc48
2170987947 <2170987947@qq.com>
2021-03-13 00:24:36 +0800

    非递归前序和中序遍历

--- structure/src/MyTree/Traversal/InTraversal.java
+++ structure/src/MyTree/Traversal/InTraversal.java
@@ -12,20 +12,29 @@ import java.util.LinkedList;
  */
 public class InTraversal {
     public void inTraversal1(TreeNode root) {
-        TreeNode cur = root;
         Deque<TreeNode> stack = new LinkedList<>();
-        if (root == null) {
-            return;
-        }
+        // 当前节点
+        TreeNode cur = root;
+        TreeNode pre = null;
         System.out.print("中序遍历(非递归): ");
         while (!stack.isEmpty() || cur != null) {
-            if (cur != null) {
+            // 如果 cur 不为空, 将其压栈.
+            while (cur != null) {
                 stack.push(cur);
                 cur = cur.left;
+            }
+            // cur 当前为空(说明刚压入栈的栈顶元素没有左节点)
+            // 就弹出栈顶元素并输出
+            // 并将 cur 指向栈顶元素的右节点
+            TreeNode top = stack.peek();
+            if (top.right == null) {
+                pre  = stack.pop();
+                System.out.print(top.val);
+            } else if (top.right == pre) {
+                pre = stack.pop();
             } else {
-                cur = stack.pop();
-                System.out.print(cur.val);
-                cur = cur.right;
+                System.out.print(top.val);
+                cur = top.right;
             }
         }
         System.out.println();
--- structure/src/MyTree/Traversal/PreTraversal.java
+++ structure/src/MyTree/Traversal/PreTraversal.java
@@ -11,22 +11,30 @@ import java.util.LinkedList;
  * @date 2021/3/6 1:09
  */
 public class PreTraversal {
-    Deque<TreeNode> stack = new LinkedList<>();
 
     public void preTraversal1(TreeNode root) {
-        if (root == null) {
-            return;
-        }
+        Deque<TreeNode> stack = new LinkedList<>();
+        TreeNode cur = root;
+        // 记录刚被遍历过的结点
+        TreeNode pre = null;
         System.out.print("先序遍历(非递归): ");
-        this.stack.push(root);
-        while (!this.stack.isEmpty()) {
-            TreeNode node = this.stack.pop();
-            System.out.print(node.val);
-            if (node.right != null) {
-                this.stack.push(node.right);
+        while (!stack.isEmpty() || cur != null) {
+            while (cur != null) {
+                stack.push(cur);
+                System.out.print(cur.val);
+                cur = cur.left;
             }
-            if (node.left != null) {
-                this.stack.push(node.left);
+            // 当前 cur 不为空 (说明栈顶元素无左节点)
+            TreeNode top = stack.peek();
+            if (top.right == null) {
+                // 1. 如果栈顶元素无右节点, 弹出栈顶元素
+                pre = stack.pop();
+            } else if (top.right == pre) {
+                // 3. 如果栈顶元素有右节点, 并且被访问过, 就弹出
+                pre = stack.pop();
+            } else {
+                // 2. 如果栈顶元素有右节点, 将 cur 指向栈顶元素的右节点.
+                cur = top.right;
             }
         }
         System.out.println();
